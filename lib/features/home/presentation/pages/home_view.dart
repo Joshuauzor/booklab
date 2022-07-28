63f888eacbc75c5144956b9abaddb74b395ab3de
joshuauzor <joshuauzor10@gmail.com>
2022-07-28 09:16:57 +0100

    home view search

--- lib/features/home/presentation/pages/home_view.dart
+++ lib/features/home/presentation/pages/home_view.dart
@@ -20,135 +20,142 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.secondaryColor,
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Gap(20),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 25,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SvgPicture.asset(AppAssets.nav),
-                        Row(
-                          children: [
-                            SvgPicture.asset(AppAssets.notification),
-                            const Gap(16.41),
-                            Image.asset(AppAssets.avatar),
-                          ],
-                        )
-                      ],
-                    ),
-                    const Gap(21),
-                    InputField(
-                      prefix: Padding(
-                        padding: const EdgeInsets.only(
-                          right: 10,
-                        ),
-                        child: SvgPicture.asset(AppAssets.search),
-                      ),
-                      controller: _searchController,
-                      placeholder: 'Search...',
-                    ),
-                  ],
-                ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Gap(20),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 25,
               ),
-              const Gap(74),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    left: 25,
-                  ),
-                  child: Row(
-                    children: const [
-                      TopBooks(
-                        amount: '#654,42',
-                        image: AppAssets.silence,
-                      ),
-                      Gap(10),
-                      TopBooks(
-                        amount: '#504,42',
-                        image: AppAssets.bitter,
-                      ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SvgPicture.asset(AppAssets.nav),
+                      Row(
+                        children: [
+                          SvgPicture.asset(AppAssets.notification),
+                          const Gap(16.41),
+                          Image.asset(AppAssets.avatar),
+                        ],
+                      )
                     ],
                   ),
-                ),
+                  const Gap(21),
+                  InputField(
+                    prefix: Padding(
+                      padding: const EdgeInsets.only(
+                        right: 10,
+                      ),
+                      child: SvgPicture.asset(AppAssets.search),
+                    ),
+                    controller: _searchController,
+                    placeholder: 'Search...',
+                  ),
+                ],
               ),
-              const Gap(41),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 25,
-                ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextRegular(
-                      'Explore',
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    const Gap(8),
-                    TextRegular(
-                      'Browse from our list of wonderful books',
-                      fontWeight: FontWeight.w500,
-                    ),
-                    const Gap(28),
+                    const Gap(74),
                     SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: const [
-                          SizedBox(
-                            width: 89,
-                            child: BookCover(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 25,
+                        ),
+                        child: Row(
+                          children: const [
+                            TopBooks(
+                              amount: '#654,42',
                               image: AppAssets.silence,
-                              height: 121,
-                              radius: 10,
                             ),
-                          ),
-                          Gap(25),
-                          SizedBox(
-                            width: 89,
-                            child: BookCover(
+                            Gap(10),
+                            TopBooks(
+                              amount: '#504,42',
                               image: AppAssets.bitter,
-                              height: 121,
-                              radius: 10,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Gap(41),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 25,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextRegular(
+                            'Explore',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          const Gap(8),
+                          TextRegular(
+                            'Browse from our list of wonderful books',
+                            fontWeight: FontWeight.w500,
+                          ),
+                          const Gap(28),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: const [
+                                SizedBox(
+                                  width: 89,
+                                  child: BookCover(
+                                    image: AppAssets.silence,
+                                    height: 121,
+                                    radius: 10,
+                                  ),
+                                ),
+                                Gap(25),
+                                SizedBox(
+                                  width: 89,
+                                  child: BookCover(
+                                    image: AppAssets.bitter,
+                                    height: 121,
+                                    radius: 10,
+                                  ),
+                                ),
+                                Gap(25),
+                                SizedBox(
+                                  width: 89,
+                                  child: BookCover(
+                                    image: AppAssets.bitter,
+                                    height: 121,
+                                    radius: 10,
+                                  ),
+                                ),
+                                Gap(25),
+                                SizedBox(
+                                  width: 89,
+                                  child: BookCover(
+                                    image: AppAssets.bitter,
+                                    height: 121,
+                                    radius: 10,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          Gap(25),
-                          SizedBox(
-                            width: 89,
-                            child: BookCover(
-                              image: AppAssets.bitter,
-                              height: 121,
-                              radius: 10,
-                            ),
-                          ),
-                          Gap(25),
-                          SizedBox(
-                            width: 89,
-                            child: BookCover(
-                              image: AppAssets.bitter,
-                              height: 121,
-                              radius: 10,
-                            ),
-                          ),
+                          const Gap(30),
                         ],
                       ),
                     ),
-                    const Gap(30),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
